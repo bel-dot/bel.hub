eca58f3c6e769c20a3766d87e4ba26d9f0ef3791
BeliyDot <artemonchannel2005@gmail.com>
2025-01-29 20:58:10 +0200

    Create CommandOutput component and integrate it into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CommandInput from './components/CommandInput'
+import CommandOutput from './components/CommandOutput';
 import './App.css';
 
 function App() {
 
   return (
     <>
+      <CommandOutput />
       <CommandInput />
     </>
   )
---
+++ src/components/CommandOutput.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './CommandOutput.scss';
+
+class CommandOutput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div className='output-field' />
+        );
+    }
+}
+
+export default CommandOutput;
