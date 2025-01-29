93ef6242cc37b67e76503f34e06fb4b99ff512a0
BeliyDot <artemonchannel2005@gmail.com>
2025-01-29 21:00:17 +0200

    Update CommandInput component

--- src/components/CommandInput.jsx
+++ src/components/CommandInput.jsx
@@ -5,12 +5,18 @@ class CommandInput extends React.Component {
    constructor(props) {
     super(props);
    } 
+   
+   componentDidMount() {
+      window.addEventListener("load", () => {
+         document.getElementById('input-el').focus();
+      })
+   }
     
    render() {
       return (
-         <div className="input-field">
+         <div id="input-field">
             <span className="arrow">&gt;</span>
-            <input type="text" className="input-el" />
+            <input type="text" className="input-el" id="input-el" />
          </div>
       );
    }
