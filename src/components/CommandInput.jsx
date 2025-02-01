28de0dbe21731cd1052188954f33689e29b05920
BeliyDot <artemonchannel2005@gmail.com>
2025-02-01 14:07:48 +0200

    Rewrite CommandInput and CommandOutput components as function components

--- src/components/CommandInput.jsx
+++ src/components/CommandInput.jsx
@@ -1,25 +1,21 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
 import './CommandInput.scss'
 
-class CommandInput extends React.Component {
-   constructor(props) {
-    super(props);
-   } 
-   
-   componentDidMount() {
+function CommandInput() {
+   const inputEl = useRef(null);
+
+   useEffect(() => {
       window.addEventListener("load", () => {
-         document.getElementById('input-el').focus();
-      })
-   }
-    
-   render() {
-      return (
-         <div id="input-field">
-            <span className="arrow">&gt;</span>
-            <input type="text" className="input-el" id="input-el" />
-         </div>
-      );
-   }
+         inputEl.current.focus();
+      });
+   }, []);
+   
+   return (
+      <div id='input-field'>
+         <span className='arrow'>&gt;</span>
+         <input type='text' ref={inputEl} id='input-el' />
+      </div>
+   )
 }
 
 export default CommandInput;
--- src/components/CommandOutput.jsx
+++ src/components/CommandOutput.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
 import './CommandOutput.scss';
 
-class CommandOutput extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
-        return (
-            <div className='output-field' />
-        );
-    }
+//class CommandOutput extends React.Component {
+//    constructor(props) {
+//        super(props);
+//    }
+//    
+//    render() {
+//        return (
+//            <div className='output-field' />
+//        );
+//    }
+//}
+
+function CommandOutput() {
+    return (
+        <div className='output-field' />
+    );
 }
 
 export default CommandOutput;
