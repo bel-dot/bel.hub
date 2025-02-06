0cf4b2269f3019cab2b708c1e11f64e120dba480
BeliyDot <artemonchannel2005@gmail.com>
2025-02-06 15:42:03 +0200

    Add styling to the Interface and Ball components

--- src/components/pong/Interface.jsx
+++ src/components/pong/Interface.jsx
@@ -1,8 +1,9 @@
 export default function Interface() {
     return (
         <div id="pong-interface">
-            <span id="player-score" />
-            <span id="computer-score" />
+            <span id="player-score">0</span>
+            <div id="pong-line" />
+            <span id="computer-score">0</span>
         </div>
     )
 }
